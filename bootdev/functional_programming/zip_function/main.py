9ad314d1d9dd4c64befaca34e021bb64c37dc77a
mehrain <marijnrovers@live.nl>
2024-03-02 19:42:02 +0100

    Woopsie.

--- bootdev/functional_programming/zip_function/main.py
+++ bootdev/functional_programming/zip_function/main.py
@@ -7,9 +7,6 @@ valid_formats = [
     "md",
 ]
 
-# Don't edit above this line
-
-
 def pair_document_with_format(doc_names, doc_formats):
-    
-    pass
+    formatted_names = list(zip(doc_names, doc_formats))
+    return filter(lambda x: x[1] in valid_formats, formatted_names)
