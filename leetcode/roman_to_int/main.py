486e35f9d256c79cb738c1c963b3b91255985e15
mehrain <marijnrovers@live.nl>
2024-03-02 20:54:05 +0100

    first leetcode addition

---
+++ leetcode/roman_to_int/main.py
@@ -0,0 +1,14 @@
+#https://leetcode.com/problems/roman-to-integer/
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+        prev = 0
+        total = 0
+        for i in s:
+            curr = roman[i]
+            if curr > prev:
+                total += curr - 2 * prev
+            else:
+                total += curr
+            prev = curr
+        return total
