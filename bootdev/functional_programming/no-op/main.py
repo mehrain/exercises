24a6681b103940c81400272b88bf922741e69982
mehrain <marijnrovers@live.nl>
2024-03-03 15:12:31 +0100

    revisit no-op, i grasp the concept but had issues with the code

--- bootdev/functional_programming/no-op/main.py
+++ bootdev/functional_programming/no-op/main.py
@@ -1,19 +1,23 @@
+#Revisit this topic, took forever to understand the problem
 def markdown_to_text(doc_content):
-    split_document = doc_content.split("\n")
-    new_document = []
-    for line in split_document:
-        if line.startswith("#"):
-            line = line.lstrip("#")
-        new_document.append(line)
-    return astrix_remover(new_document)
+    lines = doc_content.split("\n")
 
-def astrix_remover(doc_content):
-    new_document = []
-    for line in doc_content:
-        split_line = line.split(" ")
-        new_line = []
-        for word in split_line:
-            new_line.append(word.strip("*"))
-        new_document.append(" ".join(new_line))
-    return "\n".join(new_document)
+    new_lines = []
+    for line in lines:
+        header_removed = line.lstrip("#")
+        emphasis_removed = remove_asterisks_from_words(header_removed)
+        new_lines.append(emphasis_removed)
 
+    return "\n".join(new_lines)
+
+
+def remove_asterisks_from_words(line):
+    words = line.split()
+    new_words = []
+    for word in words:
+        if len(word) > 1:
+            word = word.strip("*")
+        if len(word) == 0:
+            continue
+        new_words.append(word)
+    return " ".join(new_words)
