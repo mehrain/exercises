8baf35ddfb17c5a110501c046f7da757d36255cc
mehrain <marijnrovers@live.nl>
2024-03-03 21:55:49 +0100

    recursion on trees

--- bootdev/functional_programming/recursion_on_tree/main.py
+++ bootdev/functional_programming/recursion_on_tree/main.py
@@ -1,2 +1,8 @@
 def list_files(current_node, current_path=""):
-    pass
+    file_paths = []
+    for node, children in current_node.items():
+        if children is None:
+            file_paths.append(current_path + "/" + node)
+        else:
+            file_paths.extend(list_files(children, current_path + "/" + node))
+    return file_paths
