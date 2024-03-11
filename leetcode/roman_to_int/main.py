c7b217a0ac1ee127ab0723bcb11efc2b897b3120
mehrain <marijnrovers@live.nl>
2024-03-11 23:21:47 +0100

    pycache removal

--- leetcode/roman_to_int/main.py
+++ leetcode/roman_to_int/main.py
@@ -1,5 +1,7 @@
-#https://leetcode.com/problems/roman-to-integer/
-class Solution:
+
+def main():
+# https://leetcode.com/problems/roman-to-integer/
+
     def romanToInt(self, s: str) -> int:
         roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
         prev = 0
@@ -12,3 +14,7 @@ class Solution:
                 total += curr
             prev = curr
         return total
+
+
+if __name__ == "__main__":
+    main("M")
---
+++ leetcode/roman_to_int/test_main.py
@@ -0,0 +1,31 @@
+import unittest
+from main import *
+
+class TestSolution(unittest.TestCase):
+    def setUp(self):
+        self.solution = Solution()
+
+    def test_romanToInt(self):
+        self.assertEqual(self.solution.romanToInt("III"), 3)
+        self.assertEqual(self.solution.romanToInt("IV"), 4)
+        self.assertEqual(self.solution.romanToInt("IX"), 9)
+        self.assertEqual(self.solution.romanToInt("LVIII"), 58)
+        self.assertEqual(self.solution.romanToInt("MCMXCIV"), 1994)
+
+if __name__ == '__main__':
+    unittest.main()import unittest
+from main import Solution
+
+class TestSolution(unittest.TestCase):
+    def setUp(self):
+        self.solution = Solution()
+
+    def test_romanToInt(self):
+        self.assertEqual(self.solution.romanToInt("III"), 3)
+        self.assertEqual(self.solution.romanToInt("IV"), 4)
+        self.assertEqual(self.solution.romanToInt("IX"), 9)
+        self.assertEqual(self.solution.romanToInt("LVIII"), 58)
+        self.assertEqual(self.solution.romanToInt("MCMXCIV"), 1994)
+
+if __name__ == '__main__':
+    unittest.main()
