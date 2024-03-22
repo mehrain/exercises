04604f6238899ebcae94801721dff8a288a6112f
mehrain <marijnrovers@live.nl>
2024-03-22 23:09:27 +0100

    im starting to dislike algos

--- bootdev/algorithms/O(2^N)_Exponential/main.py
+++ bootdev/algorithms/O(2^N)_Exponential/main.py
@@ -1,14 +1,23 @@
-def power_set(input_set=None):
-    if not input_set: 
-        input_set = []
-    else:
-        output_set = []
-        for number in range(1, input_set):
-            for subset in range(input_set):
-                output_set.append(number + subset)
-                output_set.append(subset)
-        return output_set
+def power_set(input_set):
+    if len(input_set) == 0:
+        return [[]]
+
+    first_element = input_set[0]
+    remaining_set = input_set[1:]
+    subsets = power_set(remaining_set)
+
+    output_set = []
+    for subset in subsets:
+        output_set.append([first_element] + subset)
+        output_set.append(subset)
+        
+    return output_set
+
                 
+                
+                
+
+
             
             
         
