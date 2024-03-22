e051addb20363d1283eb4143d4219c1d80b9c20b
mehrain <marijnrovers@live.nl>
2024-03-22 23:31:51 +0100

    time for sorting algos

---
+++ bootdev/algorithms/travel_time_limit/main.py
@@ -0,0 +1,12 @@
+def num_countries_in_days(max_days, factor):
+    time_left = max_days
+    count = 0
+    time_in_country = 1
+
+    while time_in_country <= time_left:
+        count += 1
+        time_left -= time_in_country
+        time_in_country *= factor
+
+    return count
+        
