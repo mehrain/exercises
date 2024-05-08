2efca879fec0c2f6d50522e4119c7fe64a89fdea
mehrain <marijnrovers@live.nl>
2024-05-08 23:17:08 +0200

    bootcamp resources plus first challenges completed

---
+++ udemy/java_bootcamp/Module 1 - Introduction to Java/1. Getting Started/Challenge 1/YourInitials.java
@@ -0,0 +1,12 @@
+public class YourInitials {
+    public static void main(String[] args) {
+         // Instructions for this workbook are on Learn the Part (See the Udemy Video: Your Initials to access the link).
+         System.out.println("M     M  RRRR  ");
+         System.out.println("MM   MM  R   R ");
+         System.out.println("M M M M  R   R ");
+         System.out.println("M  M  M  RRRR  ");
+         System.out.println("M     M  R R   ");
+         System.out.println("M     M  R  R  ");
+         System.out.println("M     M  R   R ");
+    }
+}
