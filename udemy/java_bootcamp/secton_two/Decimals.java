c5b678d38009c29e87d27082c862f9c99c12e512
mehrain <marijnrovers@live.nl>
2024-05-10 00:38:00 +0200

    started math operations in java

---
+++ udemy/java_bootcamp/secton_two/Decimals.java
@@ -0,0 +1,84 @@
+public class Decimals {
+    public static void main(String[] args) {
+        double price = 10.99;
+        System.out.println(price);
+        
+        double tax = 0.08;
+        double total = price + price * tax;
+        System.out.println(total);
+        
+        double average = 78.5;
+        System.out.println(average);
+        
+        double distance = 3.5E7;
+        System.out.println(distance);
+        
+        double radius = 6371.0;
+        double circumference = 2 * Math.PI * radius;
+        System.out.println(circumference);
+        
+        double pi = Math.PI;
+        System.out.println(pi);
+        
+        double x = 10.0;
+        double y = 3.0;
+        double result = x / y;
+        System.out.println(result);
+        
+        double remainder = x % y;
+        System.out.println(remainder);
+        
+        double z = 10.0;
+        z++;
+        System.out.println(z);
+        
+        z--;
+        System.out.println(z);
+        
+        double a = 10.0;
+        double b = 3.0;
+        double c = a / b;
+        System.out.println(c);
+        
+        double d = 10.0;
+        double e = 3.0;
+        double f = d / e;
+        System.out.println(f);
+        
+        double g = 10.0;
+        double h = 3.0;
+        double i = g / h;
+        System.out.println(i);
+        
+        double j = 10.0;
+        double k = 3.0;
+        double l = j / k;
+        System.out.println(l);
+        
+        double m = 10.0;
+        double n = 3.0;
+        double o = m / n;
+        System.out.println(o);
+        
+        double p = 10.0;
+        double q = 3.0;
+        double r = p / q;
+        System.out.println(r);
+        
+        double s = 10.0;
+        double t = 3.0;
+        double u = s / t;
+        System.out.println(u);
+        
+        double v = 10.0;
+        double w = 3.0;
+        double x1 = v / w;
+        System.out.println(x1);
+        
+        double y1 = 10.0;
+        double z1 = 3.0;
+        double a1 = y1 / z1;
+        System.out.println(a1);
+    }
+    
+}
---
+++ udemy/java_bootcamp/secton_two/WholeNumbers.java
@@ -0,0 +1,7 @@
+public class WholeNumbers {
+    public static void main(String[] args) {
+        long passengers = 50000000000L;
+        System.out.println(passengers);
+    }
+    
+}
