a42fafc6722266dcc4f96be2ad3d7fb56c2eeda1
mehrain <marijnrovers@live.nl>
2024-07-21 12:27:17 +0200

    more excercises.

---
+++ bootdev/golang/chapter_one/1.3.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	messageStart := "Happy Birthday! You are now"
+	age := 21
+	messageEnd := "years old!"
+	fmt.Println(messageStart, age, messageEnd)
+}
