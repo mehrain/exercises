e683ca5e5c9b487bbce21bdf2372ce10aba88198
mehrain <marijnrovers@live.nl>
2024-07-24 21:46:32 +0200

    finished chapter one

---
+++ bootdev/golang/chapter_one/c3.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	const penniesPerText float64 = 2
+
+	// don't edit below this line
+	fmt.Printf("The type of penniesPerText is %T\n", penniesPerText)
+}
